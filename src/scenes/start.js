505ffacd932b5b45d946a9a0eebc7379ffaf5392
Jon Taylor <31380713+DevJonTaylor@users.noreply.github.com>
2022-03-11 15:03:05 -0600

    Corrected the email validation

--- src/scenes/start.js
+++ src/scenes/start.js
@@ -2,10 +2,13 @@ const QFactory = require('../Questions/QFactory');
 const {Manager} = require('../Employees');
 async function start() {
   const validateEmpty = question => question.validateEmpty;
+  const validateEmail = question => question.validate(email => /^.+@[^.].*\.[a-z]{2,}$/.test(email)
+    ? true
+    : 'Please provide a valid email address.');
   const data = await QFactory
     .input ('set id', 'What is the team manager\'s employee ID?', validateEmpty)
     .input('set name', 'What is the team manager\'s name?', validateEmpty)
-    .input('set email', 'What is the team manager\'s email address?', validateEmpty)
+    .input('set email', 'What is the team manager\'s email address?', validateEmail)
     .input('set office number', 'What is the team manager\'s office number?', validateEmpty)
     .answers;
 
